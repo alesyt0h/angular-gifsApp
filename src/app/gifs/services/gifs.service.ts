45ae1a915ad35ec92eaa7a8bb5a13326b3d6c67a
Alex Ortigosa <alexortigosa179@gmail.com>
2021-07-17 20:42:33 +0200

    gifsApp Final

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchGIFResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey      : string = 'cexJDsRKWeTtItVkqsHO1GtboZio1hct';
+  private servicioUrl : string = 'https://api.giphy.com/v1/gifs';
+  private _historial  : string[] = [];
+
+  public resultados: Gif[] = []
+
+
+  public storedNames: string[] = []
+
+
+  get historial() {
+    return [...this._historial]
+  }
+
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
+
+  buscarGifs(query: string) {
+    query = query.trim().toLocaleLowerCase()
+
+    if (!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial',JSON.stringify(this._historial));
+    }
+  
+    const params = new HttpParams()
+          .set('api_key',this.apiKey)
+          .set('q',query)
+          .set('limit','10');
+
+    // this.http.get('https://api.giphy.com/v1/gifs/search?api_key='+ this.apiKey + '&q=' + query + '&limit=10')
+    // this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+    this.http.get<SearchGIFResponse>(`${this.servicioUrl}/search`, {params})
+        .subscribe((resp) => {
+          // console.log(resp.data);
+          this.resultados = resp.data;
+          localStorage.setItem('resultados',JSON.stringify(this.resultados));
+        })
+  }
+}
+
+// Normal Fetch Method
+//
+// fetch('https://api.giphy.com/v1/gifs/search?api_key=cexJDsRKWeTtItVkqsHO1GtboZio1hct&q=putas&limit=10')
+//   .then( resp => {
+//     resp.json().then(data => {
+//       console.log(data)
+//     })
+//   })
+
+// Async Fetch Method
+//
+//   async buscarGifs(query: string) {
+//      const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=cexJDsRKWeTtItVkqsHO1GtboZio1hct&q=putas&limit=10')
+//      const data = await resp.json()
+//      console.log(data)
+//   }
